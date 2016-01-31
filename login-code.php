b4fe513908dba58366552201f341a51a182f45c8
nicbaughman <nicbaughman@users.noreply.github.com>
2016-01-31 09:32:14 -0800

    added echo of username and pass to test functionality

--- login-code.php
+++ login-code.php
@@ -2,16 +2,17 @@
 session_start();  //Starting session
 
 $error=''; // Variable To Store Error Message
-if (isset($_POST['submit'])) {
-if (empty($_POST['username']) || empty($_POST['password'])) {
+  if (isset($_POST['submit'])) {
+  if (empty($_POST['username']) || empty($_POST['password'])) {
 $error = "Username or Password is invalid";
 }
-else
+  else
 {
-$username=$_POST['username'];
-$password=$_POST['password'];
+  $username=$_POST['username'];
+  $password=$_POST['password'];
 /* My understanding is if username OR password is blank give error 
 message 'Username or Password is invalid' otherwise POST username and password */
-
+echo $username;
+echo $password;
 //TODO connect to Database
 ?>
