b0c9abe72aee26eb2d038c35d9481ba3ca98539f
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-15 20:49:40 -0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     include('login-code.php'); // Includes Login Script
     include('connectdb.php'); // Includes database connection
 
-    if(isset($_SESSION['login_user'])){
+    if(isset($_SESSION["login"])){
     header("location: profile.php");
     }
 ?>
