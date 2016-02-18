ed527f190ebd90f1ae468e109e0da3629452b013
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-17 20:52:22 -0800

    Update login-code.php
    Found from this article:
    http://www.phpgang.com/how-to-create-login-and-signup-form-in-php_377.html

--- login-code.php
+++ login-code.php
@@ -1,22 +1,44 @@
 <?php
 session_start();  //Starting session
 
-$error=''; // Variable To Store Error Message
-if (isset($_POST['submit'])) {
-    if (empty($_POST['username']) || empty($_POST['password'])) {
-        $error = "Username or Password is invalid";
-    }
-    else
+if(isset($_POST['action']))
+{          
+    if($_POST['action']=="login")
     {
-        $username=$_POST['username'];
-        $password=$_POST['password'];
-        /* My understanding is if username OR password is blank give error 
-        message 'Username or Password is invalid' otherwise POST username and password */
-        echo $username;
-        echo $password;
-        //TODO connect to Database
+        $email = mysqli_real_escape_string($connection,$_POST['email']);
+        $password = mysqli_real_escape_string($connection,$_POST['password']);
+        $strSQL = mysqli_query($connection,"select name from users where email='".$email."' and password='".md5($password)."'");
+        $Results = mysqli_fetch_array($strSQL);
+        if(count($Results)>=1)
+        {
+            $message = $Results['name']." Login Sucessfully!!";
+        }
+        else
+        {
+            $message = "Invalid email or password!!";
+        }        
+    }
+    elseif($_POST['action']=="signup")
+    {
+        $name       = mysqli_real_escape_string($connection,$_POST['name']);
+        $email      = mysqli_real_escape_string($connection,$_POST['email']);
+        $password   = mysqli_real_escape_string($connection,$_POST['password']);
+        $query = "SELECT email FROM users where email='".$email."'";
+        $result = mysqli_query($connection,$query);
+        $numResults = mysqli_num_rows($result);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
+        {
+            $message =  "Invalid email address please type a valid email!!";
+        }
+        elseif($numResults>=1)
+        {
+            $message = $email." Email already exist!!";
+        }
+        else
+        {
+            mysql_query("insert into users(name,email,password) values('".$name."','".$email."','".md5($password)."')");
+            $message = "Signup Sucessfully!!";
+        }
     }
 }
-$_SESSION["login"] = $_POST;
-echo $_SESSION["login"];
 ?>
