d4fdf195ff2d68b9bf3eaf0e46762c9ecbd80c30
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-21 08:38:37 -0800

    adding in form for ingredients and recipe name

--- src/app/recipe/add-recipe.php
+++ src/app/recipe/add-recipe.php
@@ -4,11 +4,52 @@
 ?>
 
 <html>
-  <header>
-  </header>
+    <header>
+    </header>
   
-  <body>
-      <input type="button" value="Back" onClick="window.location.href = '../home/home.php'"></input>
-      <p>Made it to the recipe page!</p>
+    <body>
+        <input type="button" value="Back" onClick="window.location.href = '../home/home.php'"></input>
+        <div id="add-recipe">
+            <form action="" method="post">
+                <p><input id="recipe-name" name="recipe-name" type="text" placeholder="Recipe Name"></p>
+            </form>
+        </div>
+            
+        <p> 
+        <input type="button" value="Add Passenger" onClick="addRow('dataTable')" /> 
+        <input type="button" value="Remove Passenger" onClick="deleteRow('dataTable')" /> 
+        <p>(All acions apply only to entries with check marked check boxes only.)</p>
+        </p>
+        
+        <table id="dataTable" class="form" border="1">
+            <tbody>
+                <tr>
+                    <p>
+                        <td >
+                            <input type="checkbox" name="chk[]" checked="checked" />
+                        </td>
+                        <td>
+                            <label>Ingredient</label>
+                            <input type="text" name="ingredient-name">
+                        </td>
+                        <td>
+                            <label for="quantity">Quantity</label>
+                            <input type="text" class="small"  name="quantity">
+                        </td>
+                        <td>
+                            <label for="measurement">Measurement</label>
+                            <select id="measurement" name="measurement">
+                                <option>....</option>
+                                <option>cup</option>
+                                <option>ounce</option>
+                                <option>tablespoon</option>
+                                <option>teaspoon</option>
+                                <option>pound</option>
+                            </select>
+                        </td>
+	                </p>
+                </tr>
+            </tbody>
+        </table>
   </body>
 </html>
