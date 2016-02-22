38ee4df2294bc4b3607a6dfb773ac3f71454f72b
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-22 05:10:49 -0800

    create POST information
    just starting to think about DB implications and how to save information into the db for each recipe & Ingredient

--- src/app/recipe/add-recipe-code.php
+++ src/app/recipe/add-recipe-code.php
@@ -1,3 +1,47 @@
-<? php
-
+<?php
+session_start();  //Starting session
+include('../../db/connectdb.php'); // Includes database connection
+if(isset($_POST['action']))
+{          
+    if($_POST['action']=="add_new")
+    {
+        $recipe      = mysqli_real_escape_string($dbconnect,$_POST['recipe']);
+        $ingredient   = mysqli_real_escape_string($dbconnect,$_POST['ingredient']);
+        $quantity   = mysqli_real_escape_string($dbconnect,$_POST['quantity']);
+        $strSQL     = mysqli_query($dbconnect,"select name from user where username='$username' and password='md5($password)'");
+        $Results    = mysqli_fetch_array($strSQL);
+        if(count($Results)>=1)
+        {
+            header('Location: ../home/home.php');
+        }
+        else
+        {
+            $message = "Invalid email or password!!";
+        }        
+    }
+    elseif($_POST['action']=="edit")
+    {
+        $name       = mysqli_real_escape_string($dbconnect,$_POST['name']);
+        $username   = mysqli_real_escape_string($dbconnect,$_POST['username']);
+        $email      = mysqli_real_escape_string($dbconnect,$_POST['email']);
+        $password   = mysqli_real_escape_string($dbconnect,$_POST['password']);
+        //$query      = "SELECT email FROM user where email='$email'";
+        //echo($query);
+        $result     = mysqli_query($dbconnect,"SELECT email FROM user where email='$email'" );
+        $numResults = mysqli_num_rows($result);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
+        {
+            $message =  "Invalid email address please type a valid email!!";
+        }
+        elseif($numResults>=1)
+        {
+            $message = $email." Email already exist!!";
+        }
+        else
+        {
+            mysqli_query($dbconnect,"INSERT into user(name,username,email,password) values('$name','$username','$email','md5($password)')");
+            $message = "Signup Sucessfully!!";
+        }
+    }
+}
 ?>
