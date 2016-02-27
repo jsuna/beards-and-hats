087ea2a6c99b8ffa67f7dd47a891ba52592ef208
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-27 07:44:54 -0800

    adding in edit code
    found from this article
    http://www.startutorial.com/articles/view/php-crud-tutorial-part-2

---
+++ src/app/recipe/edit-recipe-code.php
@@ -0,0 +1,16 @@
+<?php
+    require 'database.php';
+    $id = null;
+    if ( !empty($_GET['id'])) {
+        $id = $_REQUEST['id'];
+    }
+     
+    if ( null==$id ) {
+        header("Location: ../home/home.php");
+    } else {
+        $sql = "SELECT * FROM customers where id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+    }
+?>
