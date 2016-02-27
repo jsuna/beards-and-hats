19e99092328f832d1c0368a92f5f97dd509fc77d
nicbaughman <nicbaughman@users.noreply.github.com>
2016-02-27 14:07:45 -0800

    initial code for our table results

--- src/app/home/home-code.php
+++ src/app/home/home-code.php
@@ -1,5 +1,16 @@
 <?php
-
+  include('../../db/connectdb.php'); // Includes database connection
+  include('../header/header.php'); // Standard Header
+  
+  $sql = 'SELECT * FROM customers ORDER BY id DESC';
+           foreach ($pdo->query($sql) as $row) {
+                    echo '<tr>';
+                    echo '<td>'. $row['name'] . '</td>';
+                    echo '<td>'. $row['email'] . '</td>';
+                    echo '<td>'. $row['mobile'] . '</td>';
+                    echo '</tr>';
+           }
+- See more at: http://www.startutorial.com/articles/view/php-crud-tutorial-part-1/#sthash.v2GfBP8w.dpuf
 
 
 ?>
