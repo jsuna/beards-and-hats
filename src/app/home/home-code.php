b3238d93170241e22e35a53f1d8a8fd2b58ba48e
nicbaughman <nicbaughman@users.noreply.github.com>
2016-03-13 10:11:14 -0700

    creating a table to display data

--- src/app/home/home-code.php
+++ src/app/home/home-code.php
@@ -1,6 +1,19 @@
 <?php
   //TODO work on fucking query results
- $q = "SELECT r.*, i.* FROM recipe r INNER JOIN ingredient i ON r.id=i.recipe_id";
- $results = mysqli_query($dbconnect,$q);
+  $q = "SELECT r.*, i.* FROM recipe r INNER JOIN ingredient i ON r.id=i.recipe_id";
+  $results = mysqli_query($dbconnect,$q);
+  
+  echo "<table>";
+    while($row = mysql_fetch_array($result)){   //Creates a loop to loop through results
+      echo '<tr>';
+      echo '<td>'. $row['name'] . '</td>';
+      echo '<td>'. $row['ingredient_name'] . '</td>';
+      echo '<td>'. $row['quantity'] . '</td>';
+      echo '<td>'. $row['measurement'] . '</td>';
+      echo '</tr>';
+    }
+- See more at: http://www.startutorial.com/articles/view/php-crud-tutorial-part-1/#sthash.AyA9dOC7.dpuf
+  
+  echo "</table>"; //Close the table in HTML
  
 ?>
