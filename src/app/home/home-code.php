02b1cd8f9c0ec31a5888307f7ca6c1c7250e9946
Nic Baughman <nicbaughman@Nics-iMac.local>
2016-03-13 10:45:55 -0700

    updating querry and display

--- src/app/home/home-code.php
+++ src/app/home/home-code.php
@@ -6,19 +6,20 @@
   $p = "SELECT * FROM recipe";
   $res = mysqli_query($dbconnect,$p);
   
-  echo "<table>";
-  if ($result->num_rows > 0) {
-    
-    while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-      echo '<tr>';
-      echo '<td>'. $row['r.name'] . '</td>';
-      echo '<td>'. $row['i.ingredient'] . '</td>';
-      echo '<td>'. $row['i.quantity'] . '</td>';
-      echo '<td>'. $row['i.measurement'] . '</td>';
-      echo '</tr>';
-    }
-  }
+  
+  if ($results->num_rows > 0) {
+      echo "<table>";
+        while($row = mysqli_fetch_array($results)){   //Creates a loop to loop through results
+            echo '<tr>';
+            //echo '<td>'. $row["name"] . '</td>';
+            echo '<td>'. $row['name'] . '</td>';
+            echo '<td>'. $row['quantity'] . '</td>';
+            echo '<td>'. $row['measurement'] . '</td>';
+            echo '</tr>';
+        }
+      echo "</table>"; //Close the table in HTML
+  };
 
-  // echo "</table>"; //Close the table in HTML
+    
  
 ?>
