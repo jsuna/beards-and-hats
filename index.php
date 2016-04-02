cdbb3038d306b8f076d307f61f6e4622fb3b454f
nicbaughman <nicbaughman@users.noreply.github.com>
2016-04-02 10:45:28 -0700

    re-structure homepage

--- index.php
+++ index.php
@@ -9,23 +9,14 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<header>
-			<div class="navigation">
-			</div>
-		</header>
-		<div class="section-1"></div>
-		<div class="section-2"></div>
-			<div class="left-main"></div>
-				<h1>Test left side</h1>
-				<div class="feature-1"></div>
-				<div class="feature-2"></div>
-				<div class="feature-3"></div>
-			<div class="right-main"></div>
-				<h1>Testing right main</h1>
-				<div class="feature-1"></div>
-				<div class="feature-2"></div>
-				<div class="feature-3"></div>
-		<div class="section-3"></div>
+		<div id="header"></div>
+			<header>
+				<div class="navigation"></div>
+			</header>
+		<div id="intro"></div>
+		<div id="value"></div>
+		<div id="feature"></div>
+		<div id="social"></div>
+		<div id="footer"></div>
 	</body>
 </html>
-
